70a7f83e4f4c3b4c47eb2970fced0a69e51cb2e6
Tiago Rodrigues <rtiago450@gmail.com>
2021-07-18 18:03:31 -0300

    Commit3
    Feita a adição do exercício 2 no package de repetição.

--- src/menus/Menu_Repeticao.java
+++ src/menus/Menu_Repeticao.java
@@ -11,7 +11,7 @@ public class Menu_Repeticao {
 		System.out.println("Menu Repetição");
 		System.out.println(linha.getLinha());
 		System.out.println("1 - Faça um programa que determine e mostre os cinco primeiros multiplos de 3, considerando números maiores que 0\n"
-				+ "2 - Escreva um programa que escreva na tela de 1 até 100, de 1 em 1, 3 vezes. Na primeira vez deve usar o FOR, depois While e por ultimo o Do While.");
+				+ "2 - Faça um programa que realize a soma de binários \n.");
 		return null;
 	}
 }
